6fef852703c9bb59d0b0c93e62bf5fb172aedc4c
xgude3 <153459327+xgude3@users.noreply.github.com>
2023-12-10 23:45:22 +0300

    Update vast3.sh

--- vast3.sh
+++ vast3.sh
@@ -5,43 +5,43 @@ echo "       XENBLOCKS         "
 echo "    STARTING INSTALL     "
 echo "-------------------------"
 
-sudo apt -y upgrade > /dev/null 2>&1
+apt -y upgrade > /dev/null 2>&1
 echo "STEP 1 of 10: Completed Packages Update"
 
-sudo apt -y install ocl-icd-opencl-dev > /dev/null 2>&1
+apt -y install ocl-icd-opencl-dev > /dev/null 2>&1
 echo "STEP 2 of 10: Installed OpenCL"
 
-sudo apt -y install nano  > /dev/null 2>&1
+apt -y install nano  > /dev/null 2>&1
 echo "STEP 3 of 10: Installed Nano"
 
-sudo apt -y install cmake  > /dev/null 2>&1
+apt -y install cmake  > /dev/null 2>&1
 echo "STEP 4 of 10: Installed cMake"
 
-sudo apt -y install python3-pip > /dev/null 2>&1
+apt -y install python3-pip > /dev/null 2>&1
 echo "STEP 5 of 10: Installed Python"
 
-sudo git clone https://github.com/shanhaicoder/XENGPUMiner.git > /dev/null 2>&1
+git clone https://github.com/shanhaicoder/XENGPUMiner.git > /dev/null 2>&1
 echo "STEP 6 of 10: Cloned https://github.com/shanhaicoder/XENGPUMiner.git"
 
 cd XENGPUMiner
-sudo chmod +x build.sh > /dev/null 2>&1
-sudo ./build.sh > /dev/null 2>&1
+chmod +x build.sh > /dev/null 2>&1
+./build.sh > /dev/null 2>&1
 echo "STEP 7 of 10: Permissions set!"
 
-sudo sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xF2cb272cDe0Fd29986de9a3ce50210cC364b270c/g' config.conf > /dev/null 2>&1
+sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xF2cb272cDe0Fd29986de9a3ce50210cC364b270c/g' config.conf > /dev/null 2>&1
 echo "STEP 8 of 10: Replaced ETH address"
 
-sudo pip install -U -r requirements.txt > /dev/null 2>&1
+pip install -U -r requirements.txt > /dev/null 2>&1
 echo "STEP 9 of 10: Installed Python Requirements"
 
 echo "STEP 10 of 10: Starting Miner & GPU"
-sudo nohup python3 miner.py --gpu=true > miner.log 2>&1 &
+nohup python3 miner.py --gpu=true > miner.log 2>&1 &
 sleep 1
-sudo nohup ./xengpuminer -d0 > xengpuminer-0.log 2>&1 &
+nohup ./xengpuminer -d0 > xengpuminer-0.log 2>&1 &
 sleep 1
-sudo nohup ./xengpuminer -d1 > xengpuminer-1.log 2>&1 &
+nohup ./xengpuminer -d1 > xengpuminer-1.log 2>&1 &
 sleep 1
-sudo nohup ./xengpuminer -d2 > xengpuminer-2.log 2>&1 &
+nohup ./xengpuminer -d2 > xengpuminer-2.log 2>&1 &
 
 echo "-------------------------"
 echo "    MINING  XENBLOCKS    "
